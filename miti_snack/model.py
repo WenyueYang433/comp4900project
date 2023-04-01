dd6548bbad725f17ef15913be397580b61efd9c6
WenyueYang433 <118648228+WenyueYang433@users.noreply.github.com>
2023-04-01 18:07:58 -0400

    Update model.py

--- miti_snack/model.py
+++ miti_snack/model.py
@@ -32,40 +32,44 @@ class SarsaTrainer:
         self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
         self.criterion = nn.MSELoss()
 
-    def train_step(self, state, action, reward, next_state, done):
+    def train_step(self, state, action, reward, next_state,next_action, done,num):
         state = torch.tensor(state, dtype=torch.float)
         next_state = torch.tensor(next_state, dtype=torch.float)
         action = torch.tensor(action, dtype=torch.long)
+        next_action = torch.tensor(next_action, dtype=torch.long)
         reward = torch.tensor(reward, dtype=torch.float)
         # (n, x)
-
+        
         if len(state.shape) == 1:
             # (1, x)
             state = torch.unsqueeze(state, 0)
             next_state = torch.unsqueeze(next_state, 0)
             action = torch.unsqueeze(action, 0)
+            next_action = torch.unsqueeze(next_action, 0)
+            next_state=torch.unsqueeze(next_state, 0)
             reward = torch.unsqueeze(reward, 0)
             done = (done, )
 
         # 1: predicted Q values with current state
         pred = self.model(state)
-
+        
         target = pred.clone()
         for idx in range(len(done)):
             Q_new = reward[idx]
             if not done[idx]:
-                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+                if(num==1):
+                    Q_new = reward[idx] + self.gamma * self.model(next_state[idx][0])[torch.argmax(next_action)]
+                else:
+                    Q_new = reward[idx] + self.gamma * self.model(next_state[idx])[torch.argmax(next_action)]
 
             target[idx][torch.argmax(action[idx]).item()] = Q_new
-    
-        # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
-        # pred.clone()
-        # preds[argmax(action)] = Q_new
+
         self.optimizer.zero_grad()
         loss = self.criterion(target, pred)
         loss.backward()
 
         self.optimizer.step()
+
 
 
 
